86b442524ca797e7876ab4dcfb176646d0ba343c
Julio César de Souza <134759649+Dom-Furia@users.noreply.github.com>
2024-02-19 15:58:37 -0400

    Add files via upload

--- Projeto_1/APP_1/urls.py
+++ Projeto_1/APP_1/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from . import views
+from APP_1 import views
+from APP_1.views import register, user_login, user_logout, create_post, edit_post, delete_post
+
 urlpatterns = [
- path('', views.index, name='home'),
- path('login/', views.user_login, name='login'),
- path('signup/', views.user_signup,name='signup'),
- path('logout/', views.user_logout,name='logout'),
+    path('register/',register, name='register'),
+    path('login/',user_login, name='login'),
+    path('logout/',user_logout, name='logout'),
+    path('create_post/',create_post, name='create_post'),
+    path('edit_post/<int:post_id>/',edit_post, name='edit_post'),
+    path('delete_post/<int:post_id>/',delete_post, name='delete_post'),  
 ]
--- Projeto_1/APP_1/views.py
+++ Projeto_1/APP_1/views.py
@@ -1,57 +1,84 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Post
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Comentario
+from .models import Post, Comentario
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import UserCreationForm, LoginForm
+from django.utils import timezone
 
-# Create your views here.
 def home(request):
- posts = Post.objects.all()
- return render(request,'home.html', {'posts':posts})
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    if request.method == 'POST':
+    if request.method == 'POST' and request.user.is_authenticated:  
         conteudo = request.POST.get('conteudo')
-        comentario = Comentario(nome=nome, email=email, conteudo=conteudo, post=post)
+        comentario = Comentario(nome=request.user.username, email=request.user.email, conteudo=conteudo, post=post)
         comentario.save()
         return redirect('post_detail', slug=post.slug)
-    
     return render(request, 'post_details.html', {'post': post})
-def index(request):
- return render(request, 'index.html')
 
-####### signup page ###############
-def user_signup(request):
- if request.method == 'POST':
-   form = UserCreationForm(request.POST)
-   if form.is_valid():
-     form.save()
-     return redirect('login')
- else:
-     form = UserCreationForm()
- return render(request, 'signup.html', {'form': form})
- 
-########## Pagina de Login/Entrar ########
+def register(request):
+    if request.method == 'POST':    
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
 def user_login(request):
-  if request.method == 'POST':
-    form = LoginForm(request.POST)
-    if form.is_valid():
-      username = form.cleaned_data['username']
-      password = form.cleaned_data['password']
-      user = authenticate(request, username=username, password=password)
-      if user:
-        login(request, user)
-        return redirect('home')
-  else:
-    form = LoginForm()
-  return render(request, 'login.html', {'form': form})
-    
-######## Pagina de logout/Sair ##########
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
 def user_logout(request):
- logout(request)
- return redirect('login')
- 
- 
+    logout(request)
+    return redirect('login')
+
+def create_post(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        conteudo = request.POST.get('conteudo')
+        autor = request.POST.get('autor') 
+        resumo = request.POST.get('resumo') 
+        slug = request.POST.get('slug')  
+        img_url = request.POST.get('img_url') 
+        publicado_em = timezone.now()
+        post = Post(titulo=titulo, conteudo=conteudo, autor=autor, resumo=resumo, slug=slug, img_url=img_url, publicado_em=publicado_em)
+        post.save()
+        return redirect('home')
+    return render(request, 'create_post.html')
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        post.titulo = request.POST.get('titulo')
+        post.conteudo = request.POST.get('conteudo')
+        post.autor = request.POST.get('autor')
+        post.resumo = request.POST.get('resumo')
+        post.slug = request.POST.get('slug')
+        post.img_url = request.POST.get('img_url')
+        post.save()
+        return redirect('home')
+    else:
+        return render(request, 'edit_post.html', {'post': post})
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    else:
+        return render(request, 'delete_post.html', {'post': post})
