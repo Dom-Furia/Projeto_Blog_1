e65c6e39661dd6a4ab714e6826d45658f7a754bf
Julio César de Souza <134759649+Dom-Furia@users.noreply.github.com>
2024-02-19 21:44:13 -0400

    Atualizacao

--- Projeto_1/APP_1/urls.py
+++ Projeto_1/APP_1/urls.py
@@ -1,4 +1,5 @@
 from APP_1 import views
+from django.urls import path
 from APP_1.views import register, user_login, user_logout, create_post, edit_post, delete_post
 
 urlpatterns = [
@@ -7,5 +8,5 @@ urlpatterns = [
     path('logout/',user_logout, name='logout'),
     path('create_post/',create_post, name='create_post'),
     path('edit_post/<int:post_id>/',edit_post, name='edit_post'),
-    path('delete_post/<int:post_id>/',delete_post, name='delete_post'),  
+    path('delete_post/<int:post_id>/',delete_post,  name='delete_post'),  
 ]
--- Projeto_1/Projeto_1/urls.py
+++ Projeto_1/Projeto_1/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from APP_1 import views
+from APP_1 import urls
 from django.conf import settings
 from django.conf.urls.static import static
-
+from APP_1.views import register, user_login, user_logout, create_post, edit_post, delete_post 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
